ab1b192f69d673c593a7377dda1d16b6ee5a08da
Enoch Chu <git@enochchu.com>
2014-12-27 23:02:34 -0800

    generatejson.py

---
+++ generatejson.py
@@ -0,0 +1,40 @@
+import json
+import os
+
+GIF_FOLDER="gif"
+BACK_ONE_FOLDER=".."
+
+class GenerateFolderStructure:
+	def writeJSON(self, data):
+		f = open('data.json', 'w')
+		f.write(json.dumps(data))
+		f.close()
+
+		pass
+
+
+	def generateListByFolder(self, folder):
+		foldername = folder
+
+		os.chdir(folder)
+		result = os.listdir()
+		os.chdir(BACK_ONE_FOLDER)
+
+		return result
+
+	def init(self):
+		os.chdir(GIF_FOLDER)
+		folders = os.listdir()
+		data = {}
+
+		for f in folders:
+			checkDir = os.path.isdir(f)
+
+			if (checkDir == True):
+				i = self.generateListByFolder(f)
+				data[f] = i
+
+		self.writeJSON(data)
+		pass
+
+GenerateFolderStructure().init()
